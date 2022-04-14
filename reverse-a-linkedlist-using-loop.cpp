2e247b5e911b8ccba237c9886fe938af42e56b6c
APURVA RAWAL <83977269+apurvarawal@users.noreply.github.com>
2022-04-15 02:04:49 +0530

    Update reverse-a-linkedlist-using-loop.cpp

--- reverse-a-linkedlist-using-loop.cpp
+++ reverse-a-linkedlist-using-loop.cpp
@@ -30,51 +30,6 @@ void insertAtHead(node* &head, int data){
     head = n;
 }
 
-void insertAtTail(node* &head, int data){
-    if(head==NULL){
-        head = new node(data);
-        return;
-    }
-    node* tail = head;
-    while(tail->next!=NULL){ //we are moving the tail in the backward direction
-        tail=tail->next;;
-    }
-    tail->next = new node(data);
-    return;
-}
-
-int length(node* head){ //since we are not concerned with the value at head, we will pass the node by value.
-    int cnt=0;
-    while(head!=NULL){
-        cnt++;
-        head=head->next;
-    }
-    return cnt;
-}
-
-void insertInMiddle(node* &head, int data, int position){
-    //corner case
-    if(head==NULL || position==0){
-        insertAtHead(head,data);
-    }
-    else if(position>length(head)){
-        insertAtTail(head,data);
-    }
-    else{
-        //take position-1 jumps
-        int jump=1;
-        node* temp = head; // initially
-        while(jump<=position-1){
-            temp=temp->next;
-            jump++;
-        }
-        node* n = new node(data);
-        n->next=temp->next; //we need to pass the address of the node which is present after temp, 
-                            //since it's address is stored in temp->next, we will write this.
-        temp->next=n;
-    }
-}
-
 void print(node* head){
     while(head!=NULL){
         cout<<head -> data<<" -> ";
@@ -104,6 +59,8 @@ istream& operator>>(istream &k,node*&head){ //since we have to modify the head, 
     head=inputdata();
     return k;
 }
+
+//O(N) time and O(1) stack (more efficient than recursive)
 
 void reverselinkedlist(node*&head){
     node* prev = NULL;
